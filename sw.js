e187008803c9f6493e2f02efff709333102ddaf9
guodong <xwrite@163.com>
2025-04-25 16:12:04 +0800

    fix

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-// 空文件或添加 install 事件监听（仅占位）
 self.addEventListener('install', (event) => {
-  // 空逻辑，仅确保 Service Worker 能注册成功
   console.log('sw.js install');
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('sw.js fetch');
 });
